6f6c27caa68c40940b480ceabe5e2e0f2f1ab297
Matias <matias.lazos@gmail.com>
2020-12-22 01:15:42 -0300

    adding react

---
+++ src/components/Atributtion.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Atributtion = () => {
+  return (
+    <div class="attribution">
+      Challenge by{" "}
+      <a href="https://www.frontendmentor.io/" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by <a href="https://github.com/mlazos7">Matias Lazos</a>.
+    </div>
+  );
+};
